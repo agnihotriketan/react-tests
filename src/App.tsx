cdb622fc1a5dd56f63a43cd6de03f86765762b27
Ketan Agnihotri <agnihotriketan@users.noreply.github.com>
2024-02-13 23:43:06 +0530

    learning test cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+import ExternalAPIComponent from "./externalComponent/ExternalAPIComponent";
 
 function App() {
+  const handleLogin = (username: string, password: string) => {
+    alert("Login Attempt:" + username + password);
+    // Here, you can add logic to handle the login attempt, such as calling an API.
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Welcome to home</div>
+      <>
+        <LoginForm onLogin={handleLogin} />
+      </>
+      <>
+        <ExternalAPIComponent />
+      </>
     </div>
   );
 }
---
+++ src/externalComponent/ExternalAPIComponent.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+const ExternalAPIComponent = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+          throw new Error('Failed to fetch');
+        }
+        const data = await response.json();
+        setPosts(data.slice(0, 5));
+      } catch (error:any) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default ExternalAPIComponent;
